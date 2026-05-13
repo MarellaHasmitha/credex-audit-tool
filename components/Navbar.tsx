f8a5eeae170eaf54691e803373942a01c2553240
Hasmitha Marella <hasmithamarella02@gmail.com>
2026-05-13 16:02:00 +0530

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,11 +7,9 @@ export default function Navbar(){
                 Credex Dashboard
             </h1>
 
-            <Link href="/audit">
             <button className="bg-blue-500 rounded px-3 py-1 text-white font-bold hover:bg-blue-600 cursor-pointer">
                 GitHub
             </button>
-            </Link>
 
         </nav>
     )

57836e0c9d3ba7dd8038b8547b78e2fd1901a86c
Hasmitha Marella <hasmithamarella02@gmail.com>
2026-05-13 15:59:03 +0530

    updated navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Navbar(){
     return (
         <nav className="flex justify-between items-center p-5 bg-gray-200">
@@ -5,9 +7,11 @@ export default function Navbar(){
                 Credex Dashboard
             </h1>
 
+            <Link href="/audit">
             <button className="bg-blue-500 rounded px-3 py-1 text-white font-bold hover:bg-blue-600 cursor-pointer">
                 GitHub
             </button>
+            </Link>
 
         </nav>
     )
